7c1452728a6ff5cb15782cf3cb25bf8f592d71fc
LuBani <lsca_lubani@hotmail.com>
2025-07-22 17:10:05 -0600

    cambios

--- public/connections/conexion.php
+++ public/connections/conexion.php
@@ -1,16 +1,18 @@
 <?php
 //PRODUCCIÓN
-/*
+
 $hostname = "34.55.77.19";
 $database = "rentas";
 $username = "sondeadmin";
 $password = "srk142536";
-*/
+
 
 //LOCAL
+/*
 $hostname = "127.0.0.1";
 $database = "rentas";
 $username = "root";
 $password = "cuervo";
+*/
 
 ?>
